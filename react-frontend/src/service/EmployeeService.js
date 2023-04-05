07bde6d10a114947c073fd778486749762821b25
okankaradeniz6 <okankaradeniz6@gmail.com>
2023-04-06 00:18:41 +0300

    Add react-frontend api

---
+++ react-frontend/src/service/EmployeeService.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const EMPLOYEE_SERVICE_BASE_URL = "http://localhost:9191/api/employee";
+const EMPLOYEE_ID = 3;
+
+class EmployeeService{
+    
+    getEmployee(){
+        return axios.get(EMPLOYEE_SERVICE_BASE_URL + '/' + EMPLOYEE_ID);
+    }
+}
+export default new EmployeeService
